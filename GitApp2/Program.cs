e8359099999a1679dc8fca87eae7d9b1e9cb25c7
Garrus20 <garruswasab@gmail.com>
2019-09-20 12:38:25 +0200

    Hibakeresés és leállási szöveg hozzáadva

--- GitApp2/Program.cs
+++ GitApp2/Program.cs
@@ -12,13 +12,37 @@ namespace GitApp2
         static void Bekeres()
         {
             Console.WriteLine("Hány számot szeretne megadni?");
-            int darabszam = Convert.ToInt32(Console.ReadLine());
+            string darabszam_keres = Console.ReadLine();
+            var isNumeric = int.TryParse(darabszam_keres, out int darabszam);
+            if (darabszam == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nem adhat meg nullát, a program leáll!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+            Hibaellenorzes(isNumeric);
             Console.WriteLine("Kérem adjon meg {0} számot!", darabszam);
             for (int i = 0; i < darabszam; i++)
             {
                 Console.WriteLine(i+1 + ".");
-                int j = Convert.ToInt32(Console.ReadLine());
+                string bekert_ertek = Console.ReadLine();
+                var isNumeric2 = int.TryParse(bekert_ertek, out int j);
+                Hibaellenorzes(isNumeric2);
+
                 szamok.Add(j);
+            }
+        }
+        static void Hibaellenorzes(bool hiba)
+        {
+            if (hiba == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Nem számot adott meg, a program leáll!");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                Environment.Exit(0);
             }
         }
         static void Kereses()
