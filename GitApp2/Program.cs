1c8e27df4c12e4a56a13ad918c6aa8b17b9f3807
Garrus20 <garruswasab@gmail.com>
2019-09-20 12:07:23 +0200

    10 szám bekérése és legnagyobb keresése

--- GitApp2/Program.cs
+++ GitApp2/Program.cs
@@ -8,8 +8,28 @@ namespace GitApp2
 {
     class Program
     {
+        static List<int> szamok = new List<int>();
+        static void Bekeres()
+        {
+            Console.WriteLine("Kérem adjon meg 10 számot!");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i+1 + ".");
+                int j = Convert.ToInt32(Console.ReadLine());
+                szamok.Add(j);
+            }
+        }
+        static void Kereses()
+        {
+            szamok.Sort();
+            int seged = szamok[szamok.Count - 1];
+            Console.WriteLine(seged);
+        }
         static void Main(string[] args)
         {
+            Bekeres();
+            Kereses();
+            Console.ReadKey();
         }
     }
 }
